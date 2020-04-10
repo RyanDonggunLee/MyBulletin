739febeaa729f9d1d64fdd30efa231dd09bc7db2
donggunLee-DG <donggun.lee.01@gmail.com>
2020-04-10 14:40:06 -0400

    initial_server-side

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-})
-
-app.use((req, res, next) => {
-  res.send('Hello from express!');
-})
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: 'asdf12asd',
+      title: 'First server-side post',
+      content: 'This is coming from backend'},
+    {
+      id: 'asfwasdf12',
+      title: 'Second server-side post',
+      content: 'This is coming from backend!'}
+  ];
+  res.status(200).json({
+    message: 'Posts feched successfully',
+    posts: posts
+  });
+});
 
 module.exports = app;
