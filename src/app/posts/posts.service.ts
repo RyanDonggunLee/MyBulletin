f982ff48b5d03c156e08396c1ba2698077bc209a
donggunLee-DG <donggun.lee.01@gmail.com>
2020-04-11 22:22:57 -0400

    httpclient

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
 import { Post } from './post.model';
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post [] = [];
   private postUpdated = new Subject<Post[]>();
+
+  constructor(private http: HttpClient) {
+
+  }
 
   getPost() {
    return [...this.posts];
